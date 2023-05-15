ed0f0c6462e24c5afcf38e80f6a7f77287db6dce
thuliu-yt16 <lytlogic@gmail.com>
2023-05-15 19:21:48 +0800

    dreamfusion scripts

--- scripts/dreamfusion-if-test-1.sh
+++ scripts/dreamfusion-if-test-1.sh
@@ -2,10 +2,8 @@ set -e
 gpu=1
 batch_size=4
 
-prompts=("\"a 20-sided dice made out of glass\"" "lib:dslr_frazer" "lib:lion_head" "lib:dumplings" "lib:teapot")
-# loop prompts
-for prompt in "${prompts[@]}"
-do
-    echo "Prompt: $prompt"
-    python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=$prompt data.batch_size=$batch_size
-done
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:eiffel data.batch_size=$batch_size
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:teddy_selfie data.batch_size=$batch_size
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:frosting data.batch_size=$batch_size
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:anvil data.batch_size=$batch_size
+python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:liberty data.batch_size=$batch_size
