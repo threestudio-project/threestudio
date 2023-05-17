b9a948f17a979d6e7f03f64a449a8314f83b474d
bennyguo <bennyguo@163.com>
2023-05-17 22:52:27 +0800

    remove exp scripts

--- scripts/dreamfusion-if-test-1.sh
+++
@@ -1,9 +0,0 @@
-set -e
-gpu=1
-batch_size=4
-
-python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:eiffel data.batch_size=$batch_size
-python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:teddy_selfie data.batch_size=$batch_size
-python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:frosting data.batch_size=$batch_size
-python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:anvil data.batch_size=$batch_size
-python launch.py --config configs/dreamfusion-if.yaml --train --gpu $gpu system.prompt_processor.prompt=lib:liberty data.batch_size=$batch_size
