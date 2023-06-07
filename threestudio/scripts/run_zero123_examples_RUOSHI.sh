b4b9a03efd25bb9575dc654715de3dbf43ad90f8
Vikram Voleti <vikram@ip-26-0-129-82.ec2.internal>
2023-06-07 18:16:03 +0000

    Adds script

---
+++ threestudio/scripts/run_zero123_examples_RUOSHI.sh
@@ -0,0 +1,5 @@
+python launch.py --config configs/zero123.yaml --train --gpu 0 data.image_path=/path/to/minion_rgba.png system.loggers.wandb.enable=true system.loggers.wandb.project="voletiv-zero123xl" system.freq.guidance_eval=13
+
+python launch.py --config configs/zero123.yaml --train --gpu 0 data.image_path=/path/to/minion_rgba.png system.loggers.wandb.enable=true system.loggers.wandb.project="voletiv-zero123xl" system.freq.guidance_eval=13 system.guidance.pretrained_model_name_or_path="/admin/home-vikram/zero123xl/20230605_last.ckpt" tag='XL2_${data.random_camera.height}_${rmspace:${basename:${data.image_path}},_}'
+
+python launch.py --config configs/zero123.yaml --train --gpu 0 system.loggers.wandb.enable=true system.loggers.wandb.project="voletiv-zero123xl" data.image_path=./load/images/minion_rgba.png system.freq.guidance_eval=13 system.guidance.pretrained_model_name_or_path="/admin/home-vikram/zero123xl/20230605_last.ckpt" tag='XL2_${data.random_camera.height}_${rmspace:${basename:${data.image_path}},_}'
