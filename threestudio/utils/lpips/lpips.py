6909c6f403ce11027d623d3a44db8d3ae0638aa5
DSaurus <2238454358@qq.com>
2023-06-09 18:06:30 +0800

    modify lpip save path

--- threestudio/utils/lpips/lpips.py
+++ threestudio/utils/lpips/lpips.py
@@ -25,7 +25,7 @@ class LPIPS(nn.Module):
             param.requires_grad = False
 
     def load_from_pretrained(self, name="vgg_lpips"):
-        ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
+        ckpt = get_ckpt_path(name, "threestudio/utils/lpips")
         self.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
         print("loaded pretrained LPIPS loss from {}".format(ckpt))
 
