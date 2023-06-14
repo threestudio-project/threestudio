0a4237fbaaf9d1a4563345b55d6e9f991cb0e758
Guan Luo <50522057+logan0601@users.noreply.github.com>
2023-06-14 13:51:39 +0800

    PerpNeg & Prompt Debiasing (#98)
    * implement perpneg, guess parameters for interpolation function
    
    * experiment perpneg on 2d
    
    * test perp neg
    
    * save script
    
    * try 2d
    
    * ignore ckpt
    
    * finish debugging perp-neg
    
    * finish perp-neg debug
    
    * finish perpneg debug
    
    * update readme and doc; minor fixes
    
    * fix prompt debiasing; support manually set view-dependent prompts
    
    * update readme
    
    ---------
    
    Co-authored-by: thuliu-yt16 <lytlogic@gmail.com>
    Co-authored-by: logan <loganthu2018@gmail.com>
    Co-authored-by: bennyguo <bennyguo@163.com>

--- threestudio/models/prompt_processors/base.py
+++ threestudio/models/prompt_processors/base.py
@@ -1,15 +1,18 @@
 import json
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import torch
 import torch.multiprocessing as mp
 import torch.nn as nn
+import torch.nn.functional as F
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
+from transformers import AutoTokenizer, BertForMaskedLM
 
 import threestudio
 from threestudio.utils.base import BaseObject
 from threestudio.utils.misc import barrier, cleanup, get_rank
+from threestudio.utils.ops import shifted_cosine_decay, shifted_expotional_decay
 from threestudio.utils.typing import *
 
 
@@ -39,6 +42,11 @@ class PromptProcessorOutput:
     uncond_text_embeddings_vd: Float[Tensor, "Nv N Nf"]
     directions: List[DirectionConfig]
     direction2idx: Dict[str, int]
+    use_perp_neg: bool
+    perp_neg_f_sb: Tuple[float, float, float]
+    perp_neg_f_fsb: Tuple[float, float, float]
+    perp_neg_f_fs: Tuple[float, float, float]
+    perp_neg_f_sf: Tuple[float, float, float]
 
     def get_text_embeddings(
         self,
@@ -46,7 +54,7 @@ class PromptProcessorOutput:
         azimuth: Float[Tensor, "B"],
         camera_distances: Float[Tensor, "B"],
         view_dependent_prompting: bool = True,
-    ):
+    ) -> Float[Tensor, "BB N Nf"]:
         batch_size = elevation.shape[0]
 
         if view_dependent_prompting:
@@ -69,9 +77,96 @@ class PromptProcessorOutput:
         # IMPORTANT: we return (cond, uncond), which is in different order than other implementations!
         return torch.cat([text_embeddings, uncond_text_embeddings], dim=0)
 
+    def get_text_embeddings_perp_neg(
+        self,
+        elevation: Float[Tensor, "B"],
+        azimuth: Float[Tensor, "B"],
+        camera_distances: Float[Tensor, "B"],
+        view_dependent_prompting: bool = True,
+    ) -> Tuple[Float[Tensor, "BBBB N Nf"], Float[Tensor, "B 2"]]:
+        assert (
+            view_dependent_prompting
+        ), "Perp-Neg only works with view-dependent prompting"
+
+        batch_size = elevation.shape[0]
+
+        direction_idx = torch.zeros_like(elevation, dtype=torch.long)
+        for d in self.directions:
+            direction_idx[
+                d.condition(elevation, azimuth, camera_distances)
+            ] = self.direction2idx[d.name]
+        # 0 - side view
+        # 1 - front view
+        # 2 - back view
+        # 3 - overhead view
+
+        pos_text_embeddings = []
+        neg_text_embeddings = []
+        neg_guidance_weights = []
+        uncond_text_embeddings = []
+
+        side_emb = self.text_embeddings_vd[0]
+        front_emb = self.text_embeddings_vd[1]
+        back_emb = self.text_embeddings_vd[2]
+        overhead_emb = self.text_embeddings_vd[3]
+
+        for idx, ele, azi, dis in zip(
+            direction_idx, elevation, azimuth, camera_distances
+        ):
+            azi = shift_azimuth_deg(azi)  # to (-180, 180)
+            uncond_text_embeddings.append(
+                self.uncond_text_embeddings_vd[idx]
+            )  # should be ""
+            if idx.item() == 3:  # overhead view
+                pos_text_embeddings.append(overhead_emb)  # side view
+                # dummy
+                neg_text_embeddings += [
+                    self.uncond_text_embeddings_vd[idx],
+                    self.uncond_text_embeddings_vd[idx],
+                ]
+                neg_guidance_weights += [0.0, 0.0]
+            else:  # interpolating views
+                if torch.abs(azi) < 90:
+                    # front-side interpolation
+                    # 0 - complete side, 1 - complete front
+                    r_inter = 1 - torch.abs(azi) / 90
+                    pos_text_embeddings.append(
+                        r_inter * front_emb + (1 - r_inter) * side_emb
+                    )
+                    neg_text_embeddings += [front_emb, side_emb]
+                    neg_guidance_weights += [
+                        -shifted_expotional_decay(*self.perp_neg_f_fs, r_inter),
+                        -shifted_expotional_decay(*self.perp_neg_f_sf, 1 - r_inter),
+                    ]
+                else:
+                    # side-back interpolation
+                    # 0 - complete back, 1 - complete side
+                    r_inter = 2.0 - torch.abs(azi) / 90
+                    pos_text_embeddings.append(
+                        r_inter * side_emb + (1 - r_inter) * back_emb
+                    )
+                    neg_text_embeddings += [side_emb, front_emb]
+                    neg_guidance_weights += [
+                        -shifted_expotional_decay(*self.perp_neg_f_sb, r_inter),
+                        -shifted_expotional_decay(*self.perp_neg_f_fsb, r_inter),
+                    ]
+
+        text_embeddings = torch.cat(
+            [
+                torch.stack(pos_text_embeddings, dim=0),
+                torch.stack(uncond_text_embeddings, dim=0),
+                torch.stack(neg_text_embeddings, dim=0),
+            ],
+            dim=0,
+        )
+
+        return text_embeddings, torch.as_tensor(
+            neg_guidance_weights, device=elevation.device
+        ).reshape(batch_size, 2)
+
 
 def shift_azimuth_deg(azimuth: Float[Tensor, "..."]) -> Float[Tensor, "..."]:
-    # shift azimuth angle (in degrees), to [180, 180]
+    # shift azimuth angle (in degrees), to [-180, 180]
     return (azimuth + 180) % 360 - 180
 
 
@@ -79,6 +174,12 @@ class PromptProcessor(BaseObject):
     @dataclass
     class Config(BaseObject.Config):
         prompt: str = "a hamburger"
+
+        # manually assigned view-dependent prompts
+        prompt_side: Optional[str] = None
+        prompt_back: Optional[str] = None
+        prompt_overhead: Optional[str] = None
+
         negative_prompt: str = ""
         pretrained_model_name_or_path: str = "runwayml/stable-diffusion-v1-5"
         overhead_threshold: float = 60.0
@@ -87,6 +188,25 @@ class PromptProcessor(BaseObject):
         view_dependent_prompt_front: bool = False
         use_cache: bool = True
         spawn: bool = True
+
+        # perp neg
+        use_perp_neg: bool = False
+        # a*e(-b*r) + c
+        # a * e(-b) + c = 0
+        perp_neg_f_sb: Tuple[float, float, float] = (1, 0.5, -0.606)
+        perp_neg_f_fsb: Tuple[float, float, float] = (1, 0.5, +0.967)
+        perp_neg_f_fs: Tuple[float, float, float] = (
+            4,
+            0.5,
+            -2.426,
+        )  # f_fs(1) = 0, a, b > 0
+        perp_neg_f_sf: Tuple[float, float, float] = (4, 0.5, -2.426)
+
+        # prompt debiasing
+        use_prompt_debiasing: bool = False
+        pretrained_model_name_or_path_prompt_debiasing: str = "bert-base-uncased"
+        # index of words that can potentially be removed
+        prompt_debiasing_mask_ids: Optional[List[int]] = None
 
     cfg: Config
 
@@ -178,10 +298,36 @@ class PromptProcessor(BaseObject):
         self.prompt = self.preprocess_prompt(self.cfg.prompt)
         # use provided negative prompt
         self.negative_prompt = self.cfg.negative_prompt
+
         threestudio.info(
             f"Using prompt [{self.prompt}] and negative prompt [{self.negative_prompt}]"
         )
-        self.prompts_vd = [d.prompt(self.prompt) for d in self.directions]
+
+        # view-dependent prompting
+        if self.cfg.use_prompt_debiasing:
+            assert (
+                self.cfg.prompt_side is None
+                and self.cfg.prompt_back is None
+                and self.cfg.prompt_overhead is None
+            ), "Do not manually assign prompt_side, prompt_back or prompt_overhead when using prompt debiasing"
+            prompts = self.get_debiased_prompt(self.prompt)
+            self.prompts_vd = [
+                d.prompt(prompt) for d, prompt in zip(self.directions, prompts)
+            ]
+        else:
+            self.prompts_vd = [
+                d.prompt(self.cfg.get(f"prompt_{d.name}", None) or self.prompt)  # type: ignore
+                for d in self.directions
+            ]
+
+        prompts_vd_display = " ".join(
+            [
+                f"[{d.name}]:[{prompt}]"
+                for prompt, d in zip(self.prompts_vd, self.directions)
+            ]
+        )
+        threestudio.info(f"Using view-dependent prompts {prompts_vd_display}")
+
         self.negative_prompts_vd = [
             d.negative_prompt(self.negative_prompt) for d in self.directions
         ]
@@ -292,6 +438,66 @@ class PromptProcessor(BaseObject):
     ) -> Tuple[Float[Tensor, "B ..."], Float[Tensor, "B ..."]]:
         raise NotImplementedError
 
+    def get_debiased_prompt(self, prompt: str) -> List[str]:
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.cfg.pretrained_model_name_or_path_prompt_debiasing
+        )
+        model = BertForMaskedLM.from_pretrained(
+            self.cfg.pretrained_model_name_or_path_prompt_debiasing
+        )
+
+        views = [d.name for d in self.directions]
+        view_ids = tokenizer(" ".join(views), return_tensors="pt").input_ids[0]
+        view_ids = view_ids[1:5]
+
+        def modulate(prompt):
+            prompt_vd = f"This image is depicting a [MASK] view of {prompt}"
+            tokens = tokenizer(
+                prompt_vd,
+                padding="max_length",
+                truncation=True,
+                add_special_tokens=True,
+                return_tensors="pt",
+            )
+            mask_idx = torch.where(tokens.input_ids == tokenizer.mask_token_id)[1]
+
+            logits = model(**tokens).logits
+            logits = F.softmax(logits[0, mask_idx], dim=-1)
+            logits = logits[0, view_ids]
+            probes = logits / logits.sum()
+            return probes
+
+        prompts = [prompt.split(" ") for _ in range(4)]
+        full_probe = modulate(prompt)
+        n_words = len(prompt.split(" "))
+        prompt_debiasing_mask_ids = (
+            self.cfg.prompt_debiasing_mask_ids
+            if self.cfg.prompt_debiasing_mask_ids is not None
+            else list(range(n_words))
+        )
+        words_to_debias = [prompt.split(" ")[idx] for idx in prompt_debiasing_mask_ids]
+        threestudio.info(f"Words that can potentially be removed: {words_to_debias}")
+        for idx in prompt_debiasing_mask_ids:
+            words = prompt.split(" ")
+            prompt_ = " ".join(words[:idx] + words[(idx + 1) :])
+            part_probe = modulate(prompt_)
+
+            pmi = full_probe / torch.lerp(part_probe, full_probe, 0.5)
+            for i in range(pmi.shape[0]):
+                if pmi[i].item() < 0.95:
+                    prompts[i][idx] = ""
+
+        debiased_prompts = [" ".join([word for word in p if word]) for p in prompts]
+        for d, debiased_prompt in zip(views, debiased_prompts):
+            threestudio.info(f"Debiased prompt of the {d} view is [{debiased_prompt}]")
+
+        del tokenizer, model
+        cleanup()
+
+        return debiased_prompts
+
     def __call__(self) -> PromptProcessorOutput:
         return PromptProcessorOutput(
             text_embeddings=self.text_embeddings,
@@ -300,4 +506,9 @@ class PromptProcessor(BaseObject):
             uncond_text_embeddings_vd=self.uncond_text_embeddings_vd,
             directions=self.directions,
             direction2idx=self.direction2idx,
+            use_perp_neg=self.cfg.use_perp_neg,
+            perp_neg_f_sb=self.cfg.perp_neg_f_sb,
+            perp_neg_f_fsb=self.cfg.perp_neg_f_fsb,
+            perp_neg_f_fs=self.cfg.perp_neg_f_fs,
+            perp_neg_f_sf=self.cfg.perp_neg_f_sf,
         )
--- threestudio/utils/ops.py
+++ threestudio/utils/ops.py
@@ -416,3 +416,24 @@ class ShapeLoss(nn.Module):
             nerf_occ, indicator, weight=weight
         )  # order is important for CE loss + second argument may not be optimized
         return loss
+
+
+def shifted_expotional_decay(a, b, c, r):
+    return a * torch.exp(-b * r) + c
+
+
+def shifted_cosine_decay(a, b, c, r):
+    return a * torch.cos(b * r + c) + a
+
+
+def perpendicular_component(x: Float[Tensor, "B C H W"], y: Float[Tensor, "B C H W"]):
+    # get the component of x that is perpendicular to y
+    eps = torch.ones_like(x[:, 0, 0, 0]) * 1e-6
+    return (
+        x
+        - (
+            torch.mul(x, y).sum(dim=[1, 2, 3])
+            / torch.maximum(torch.mul(y, y).sum(dim=[1, 2, 3]), eps)
+        ).view(-1, 1, 1, 1)
+        * y
+    )
