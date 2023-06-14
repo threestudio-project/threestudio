31a1de208165fa27984943346eb00d29a6f06a12
DSaurus <2238454358@qq.com>
2023-06-14 18:48:46 +0800

    general patch renderer

--- threestudio/models/renderers/patch_renderer.py
+++ threestudio/models/renderers/patch_renderer.py
@@ -15,26 +15,15 @@ from threestudio.utils.misc import get_device
 from threestudio.utils.ops import chunk_batch
 from threestudio.utils.rasterize import NVDiffRasterizerContext
 from threestudio.utils.typing import *
-from threestudio.utils.GAN.mobilenet import MobileNetV3 as GlobalEncoder
-from threestudio.utils.GAN.vae import Decoder as Generator
-from threestudio.utils.GAN.vae import Encoder as LocalEncoder
-from threestudio.utils.GAN.distribution import DiagonalGaussianDistribution
-from threestudio.utils.GAN.discriminator import NLayerDiscriminator, weights_init
 
 
 @threestudio.register("patch-renderer")
 class PatchRenderer(VolumeRenderer):
     @dataclass
     class Config(VolumeRenderer.Config):
-        num_samples_per_ray: int = 512
-        randomized: bool = True
-        eval_chunk_size: int = 160000
-        grid_prune: bool = True
-        return_comp_normal: bool = False
-        return_normal_perturb: bool = False
-
         patch_size: int = 128
         base_renderer_type: str = ""
+        base_renderer: Optional[VolumeRenderer.Config] = None
     
     cfg: Config
     def configure(
@@ -46,7 +35,7 @@ class PatchRenderer(VolumeRenderer):
         cfg_copy = self.cfg.copy()
         del cfg_copy.patch_size
         del cfg_copy.base_renderer_type
-        self.base_renderer = threestudio.find(self.cfg.base_renderer_type)(cfg_copy, 
+        self.base_renderer = threestudio.find(self.cfg.base_renderer_type)(cfg_copy.base_renderer, 
             geometry=geometry,
             material=material,
             background=background
@@ -72,22 +61,28 @@ class PatchRenderer(VolumeRenderer):
                 rays_d.permute(0, 3, 1, 2), (H // scale_ratio, W // scale_ratio), mode='bilinear'
             ).permute(0, 2, 3, 1)
 
-            out = self.base_renderer(global_rays_o, global_rays_d, light_positions, bg_color, **kwargs)
-            comp_rgb = out["comp_rgb"]
+            out_base = self.base_renderer(global_rays_o, global_rays_d, light_positions, bg_color, **kwargs)
+            comp_rgb = out_base["comp_rgb"]
             comp_rgb = F.interpolate(comp_rgb.permute(0, 3, 1, 2), (H, W), mode='bilinear').permute(0, 2, 3, 1)
 
             patch_x = torch.randint(0, W-self.cfg.patch_size, (1,)).item()
             patch_y = torch.randint(0, H-self.cfg.patch_size, (1,)).item()
             patch_rays_o = rays_o[:, patch_y:patch_y+self.cfg.patch_size, patch_x:patch_x+self.cfg.patch_size]
             patch_rays_d = rays_d[:, patch_y:patch_y+self.cfg.patch_size, patch_x:patch_x+self.cfg.patch_size]
-
             out = self.base_renderer(patch_rays_o, patch_rays_d, light_positions, bg_color, **kwargs)
             patch_comp_rgb = out["comp_rgb"]
             comp_rgb[:, patch_y:patch_y+self.cfg.patch_size, patch_x:patch_x+self.cfg.patch_size] = patch_comp_rgb
-    
-            out.update({
-                "comp_rgb": comp_rgb
-            })
+
+            
+            if patch_x % 2 == 0:
+                out_base.update({
+                    "comp_rgb": comp_rgb
+                })
+                out = out_base
+            else:
+                out.update({
+                    "comp_rgb": comp_rgb
+                })
         else:
             out = self.base_renderer(rays_o, rays_d, light_positions, bg_color, **kwargs)
 
