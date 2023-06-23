caca9293a788f9e76059d8a37ceb0d83b0d4bceb
Christian Laforte <christian.laforte@gmail.com>
2023-06-23 19:06:18 +0000

    reorder script

--- threestudio/scripts/run_zero123_comparison.sh
+++ threestudio/scripts/run_zero123_comparison.sh
@@ -1,4 +1,7 @@
-# with zero123XL
+# with standard zero123
+threestudio/scripts/run_zero123_phase.sh 6 anya_front 105000 0
+
+# with zero123XL (not released yet!)
 threestudio/scripts/run_zero123_phase.sh 1 anya_front XL_20230604 0
 threestudio/scripts/run_zero123_phase.sh 2 baby_phoenix_on_ice XL_20230604 20
 threestudio/scripts/run_zero123_phase.sh 3 beach_house_1 XL_20230604 50
@@ -13,16 +16,8 @@ threestudio/scripts/run_zero123_phase.sh 3 futuristic_car XL_20230604 20
 threestudio/scripts/run_zero123_phase.sh 4 mona_lisa XL_20230604 10
 threestudio/scripts/run_zero123_phase.sh 5 teddy XL_20230604 20
 
-# with standard zero123
-threestudio/scripts/run_zero123_phase.sh 6 anya_front 105000 0
-
 # set guidance_eval to 0, to greatly speed up training
 threestudio/scripts/run_zero123_phase.sh 7 anya_front XL_20230604 0 system.freq.guidance_eval=0
 
 # disable wandb for faster training (or if you don't want to use it)
 threestudio/scripts/run_zero123_phase.sh 7 anya_front XL_20230604 0 system.loggers.wandb.enable=false system.freq.guidance_eval=0
-
-
-# BLA27
-# test: disable depth altogether for baby phoenix
-threestudio/scripts/run_zero123_phase.sh 0 baby_phoenix_on_ice XL_20230604 20 system.loss.lambda_depth=0.0
