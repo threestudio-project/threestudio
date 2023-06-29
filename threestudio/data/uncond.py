b32f18a1c90d7c4cd8ee72f38360a1379cbba98c
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-06-29 02:47:40 +0000

    Comments out camera_distance_range and fovy_range from progressive_view

--- threestudio/data/uncond.py
+++ threestudio/data/uncond.py
@@ -126,16 +126,16 @@ class RandomCameraIterableDataset(IterableDataset, Updateable):
             (1 - r) * 0.0 + r * self.cfg.azimuth_range[0],
             (1 - r) * 0.0 + r * self.cfg.azimuth_range[1],
         ]
-        self.camera_distance_range = [
-            (1 - r) * self.cfg.eval_camera_distance
-            + r * self.cfg.camera_distance_range[0],
-            (1 - r) * self.cfg.eval_camera_distance
-            + r * self.cfg.camera_distance_range[1],
-        ]
-        self.fovy_range = [
-            (1 - r) * self.cfg.eval_fovy_deg + r * self.cfg.fovy_range[0],
-            (1 - r) * self.cfg.eval_fovy_deg + r * self.cfg.fovy_range[1],
-        ]
+        # self.camera_distance_range = [
+        #     (1 - r) * self.cfg.eval_camera_distance
+        #     + r * self.cfg.camera_distance_range[0],
+        #     (1 - r) * self.cfg.eval_camera_distance
+        #     + r * self.cfg.camera_distance_range[1],
+        # ]
+        # self.fovy_range = [
+        #     (1 - r) * self.cfg.eval_fovy_deg + r * self.cfg.fovy_range[0],
+        #     (1 - r) * self.cfg.eval_fovy_deg + r * self.cfg.fovy_range[1],
+        # ]
 
     def collate(self, batch) -> Dict[str, Any]:
         # sample elevation angles
