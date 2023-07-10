4a8fc7de61764a06c60d371b25491bbca97ed78c
yankeesong <yankeesong1998@gmail.com>
2023-07-10 05:13:04 -0400

    typecheck for mesh input

--- threestudio/models/geometry/implicit_sdf.py
+++ threestudio/models/geometry/implicit_sdf.py
@@ -135,10 +135,12 @@ class ImplicitSDF(BaseImplicitGeometry):
             scene = trimesh.load(mesh_path)
             if isinstance(scene, trimesh.Trimesh):
                 mesh = scene
-            else:
+            elif isinstance(scene, trimesh.scene.Scene):
                 mesh = trimesh.Trimesh()
                 for obj in scene.geometry.values():
                     mesh = trimesh.util.concatenate([mesh, obj])
+            else:
+                raise ValueError(f"Unknown mesh type at {mesh_path}.")
 
             # move to center
             centroid = mesh.vertices.mean(0)
