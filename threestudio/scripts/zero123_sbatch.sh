8030092f2fc97e2654557a2a26798fdfb67a27f7
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-07-12 16:16:34 +0000

    Replaces full paths

--- threestudio/scripts/zero123_sbatch.sh
+++ threestudio/scripts/zero123_sbatch.sh
@@ -5,6 +5,6 @@
 #SBATCH --gpus=1
 #SBATCH --time=0-00:07:00
 conda activate three
-cd /admin/home-vikram/git/threestudio/
+cd ~/git/threestudio/
 NAME="dog1"
-python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png system.guidance.pretrained_model_name_or_path=./load/zero123/XL_20230604.ckpt use_timestamp=False name=${NAME}_DEMO tag=XL_Phase1 system.loggers.wandb.enable=true system.loggers.wandb.project='voletiv-zero123XL-demo-NEW2' system.loggers.wandb.name=${NAME}_XL_Phase1
+python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/${NAME}_rgba.png use_timestamp=False name=${NAME} tag=Phase1 system.loggers.wandb.enable=true system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1
