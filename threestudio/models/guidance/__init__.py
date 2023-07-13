1cd2dab2019031596bf591531b01a45f108f4a4c
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-07-13 16:29:31 +0000

    Tries to add SDXL, fails

--- threestudio/models/guidance/__init__.py
+++ threestudio/models/guidance/__init__.py
@@ -2,6 +2,7 @@ from . import (
     controlnet_guidance,
     deep_floyd_guidance,
     instructpix2pix_guidance,
+    sdxl_guidance,
     stable_diffusion_guidance,
     stable_diffusion_vsd_guidance,
     zero123_guidance,
