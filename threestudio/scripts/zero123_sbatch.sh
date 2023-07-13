3618c262d428436bb0e5f5dda78123d3b841605f
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-07-13 01:20:40 +0000

    Updates

--- threestudio/scripts/zero123_sbatch.sh
+++ threestudio/scripts/zero123_sbatch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=vikky
+#SBATCH --job-name=vikky_alien1
 #SBATCH --account=mod3d
 #SBATCH --partition=g40
 #SBATCH --gpus=1
 #SBATCH --time=0-00:07:00
 conda activate three
 cd ~/git/threestudio/
-NAME="dog1"
-python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/${NAME}_rgba.png use_timestamp=False name=${NAME} tag=Phase1 system.loggers.wandb.enable=true system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1
+NAME=alien1
+python launch.py --config configs/experimental/imagecondition_refine.yaml --train data.image_path=./load/images/${NAME}_rgba.png system.prompt_processor.prompt='A simple 3D render of an alien' system.geometry_convert_from=./outputs/${NAME}/Phase1/ckpts/last.ckpt tag=${NAME}_Phase2_refine # system.freq.guidance_eval=0 system.loggers.wandb.enable=false system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase2
