fcffdc077706e86f994467c42c0717bbf6ffffd4
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-07-26 03:50:22 +0000

    Better configs, zero123_guidance with dmitry's ckpts

--- threestudio/scripts/run_zero123_sbatch.py
+++ threestudio/scripts/run_zero123_sbatch.py
@@ -17,6 +17,17 @@ files = [
     "~/git/threestudio/load/images/temple2_rgba.png",
 ]
 
+files = [
+    "~/git/threestudio/load/images/km1_rgba.png",
+    "~/git/threestudio/load/images/km2_rgba.png",
+    "~/git/threestudio/load/images/km3_rgba.png",
+    "~/git/threestudio/load/images/km4_rgba.png",
+    "~/git/threestudio/load/images/km5_rgba.png",
+    "~/git/threestudio/load/images/km6_rgba.png",
+    "~/git/threestudio/load/images/km7_rgba.png",
+    "~/git/threestudio/load/images/km8_rgba.png",
+]
+
 for file in files:
     name = os.path.basename(file).split("_rgba.png")[0]
     with open(
@@ -34,8 +45,16 @@ for file in files:
         f.write(f"NAME={name}\n")
         # Phase 1
         f.write(
-            "python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png use_timestamp=false name=${NAME} tag=Phase1_highNoise_lr0.001fast_frontAvoid5_norm50 system.loggers.wandb.enable=false system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1\n"
+            "python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png use_timestamp=false name=SAI/${NAME} tag=Phase1_SAI_new_elev5_EXP1 system.loggers.wandb.enable=false system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1\n"
         )
+        # new, elev 10
+        # f.write(
+        #     "python launch.py --config configs/zero123.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png data.default_elevation_deg=5.0 use_timestamp=false name=${NAME} tag=Phase1_new_elev5 system.loggers.wandb.enable=false system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1\n"
+        # )
+        # original, elev 5
+        # f.write(
+        #     "python launch.py --config configs/zero123_orig.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png data.default_elevation_deg=5.0 use_timestamp=false name=${NAME} tag=Phase1_orig_elev5 system.loggers.wandb.enable=false system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1\n"
+        # )
         # # Phase 1.5
         # f.write(
         #     "python launch.py --config configs/zero123-geometry.yaml --train data.image_path=./load/images/${NAME}_rgba.png system.geometry_convert_from=./outputs/${NAME}/Phase1/ckpts/last.ckpt use_timestamp=False name=${NAME} tag=Phase1p5 system.loggers.wandb.enable=true system.loggers.wandb.project='zero123' system.loggers.wandb.name=${NAME}_Phase1p5\n"
