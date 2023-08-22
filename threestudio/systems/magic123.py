11cdb442c28f84e271b8b53ed2bd93a010b1301c
bennyguo <bennyguo@163.com>
2023-08-22 12:52:21 +0800

    magic123 refinement stage

--- threestudio/systems/magic123.py
+++ threestudio/systems/magic123.py
@@ -13,6 +13,7 @@ from threestudio.utils.typing import *
 class Magic123(BaseLift3DSystem):
     @dataclass
     class Config(BaseLift3DSystem.Config):
+        refinement: bool = False
         guidance_3d_type: str = ""
         guidance_3d: dict = field(default_factory=dict)
 
@@ -85,26 +86,40 @@ class Magic123(BaseLift3DSystem):
                     self.cfg.loss[name.replace("loss_", "lambda_3d_")]
                 )
 
-        if self.C(self.cfg.loss.lambda_orient) > 0:
-            if "normal" not in out:
-                raise ValueError(
-                    "Normal is required for orientation loss, no normal is found in the output."
+        if not self.cfg.refinement:
+            if self.C(self.cfg.loss.lambda_orient) > 0:
+                if "normal" not in out:
+                    raise ValueError(
+                        "Normal is required for orientation loss, no normal is found in the output."
+                    )
+                loss_orient = (
+                    out["weights"].detach()
+                    * dot(out["normal"], out["t_dirs"]).clamp_min(0.0) ** 2
+                ).sum() / (out["opacity"] > 0).sum()
+                self.log("train/loss_orient", loss_orient)
+                loss += loss_orient * self.C(self.cfg.loss.lambda_orient)
+
+            loss_sparsity = (out["opacity"] ** 2 + 0.01).sqrt().mean()
+            self.log("train/loss_sparsity", loss_sparsity)
+            loss += loss_sparsity * self.C(self.cfg.loss.lambda_sparsity)
+
+            opacity_clamped = out["opacity"].clamp(1.0e-3, 1.0 - 1.0e-3)
+            loss_opaque = binary_cross_entropy(opacity_clamped, opacity_clamped)
+            self.log("train/loss_opaque", loss_opaque)
+            loss += loss_opaque * self.C(self.cfg.loss.lambda_opaque)
+        else:
+            loss_normal_consistency = out["mesh"].normal_consistency()
+            self.log("train/loss_normal_consistency", loss_normal_consistency)
+            loss += loss_normal_consistency * self.C(
+                self.cfg.loss.lambda_normal_consistency
+            )
+
+            if self.C(self.cfg.loss.lambda_laplacian_smoothness) > 0:
+                loss_laplacian_smoothness = out["mesh"].laplacian()
+                self.log("train/loss_laplacian_smoothness", loss_laplacian_smoothness)
+                loss += loss_laplacian_smoothness * self.C(
+                    self.cfg.loss.lambda_laplacian_smoothness
                 )
-            loss_orient = (
-                out["weights"].detach()
-                * dot(out["normal"], out["t_dirs"]).clamp_min(0.0) ** 2
-            ).sum() / (out["opacity"] > 0).sum()
-            self.log("train/loss_orient", loss_orient)
-            loss += loss_orient * self.C(self.cfg.loss.lambda_orient)
-
-        loss_sparsity = (out["opacity"] ** 2 + 0.01).sqrt().mean()
-        self.log("train/loss_sparsity", loss_sparsity)
-        loss += loss_sparsity * self.C(self.cfg.loss.lambda_sparsity)
-
-        opacity_clamped = out["opacity"].clamp(1.0e-3, 1.0 - 1.0e-3)
-        loss_opaque = binary_cross_entropy(opacity_clamped, opacity_clamped)
-        self.log("train/loss_opaque", loss_opaque)
-        loss += loss_opaque * self.C(self.cfg.loss.lambda_opaque)
 
         for name, value in self.cfg.loss.items():
             self.log(f"train_params/{name}", self.C(value))
