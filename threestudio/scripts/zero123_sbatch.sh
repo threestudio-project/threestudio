c1e8d5b07fdbd8412dd10ce0edf82f0d23dc8ab1
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-09-17 21:02:17 +0000

    Minor update

--- threestudio/scripts/zero123_sbatch.sh
+++ threestudio/scripts/zero123_sbatch.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=vikky_yoga3
+#SBATCH --job-name=vikky_cr_ellie
 #SBATCH --account=mod3d
 #SBATCH --partition=g40
 #SBATCH --gpus=1
 #SBATCH --time=0-00:30:00
+#SBATCH --time=0-00:30:00
+source  ~/miniconda3/etc/profile.d/conda.sh
 conda activate three
 cd ~/git/threestudio/
-NAME=yoga3
-CONFIG=3sai
-MODEL=elevcond2
+NAME=cr_ellie
+CONFIG=sai_multinoise_amb_fast
+MODEL=comb1
 ELEV=5.0
-PROJECT=Zero123_Dmitry_comp
-TAG=
-WANDB=true
-python launch.py --config configs/zero123_${CONFIG}.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png system.guidance.pretrained_model_name_or_path=./load/zero123/zero123-${MODEL}.ckpt use_timestamp=false name=${PROJECT}/${NAME} tag=Phase1_${CONFIG}config_${MODEL}model_${ELEV}elev_${TAG} data.default_elevation_deg=${ELEV} system.loggers.wandb.enable=${WANDB} system.loggers.wandb.project=${PROJECT} system.loggers.wandb.name=${NAME}_${CONFIG}config_${MODEL}model_${ELEV}elev_${TAG}
+AZIM=70.0
+PROJECT=AMB
+TAG=mn14_AMB_fast
+WANDB=True
+python launch.py --config configs/zero123_${CONFIG}.yaml --train data.image_path=./load/images/fsx/${NAME}_rgba.png system.guidance.pretrained_model_name_or_path=./load/zero123/zero123-${MODEL}.ckpt use_timestamp=false name=${PROJECT}/${NAME} tag=Phase1_${CONFIG}config_${MODEL}model_${ELEV}elev_${AZIM}azim_${TAG} data.default_elevation_deg=${ELEV} data.default_azimuth_deg=${AZIM} system.loggers.wandb.enable=${WANDB} system.loggers.wandb.project=${PROJECT} system.loggers.wandb.name=${NAME}_${CONFIG}config_${MODEL}model_${ELEV}elev_${AZIM}azim_${TAG}
